f409b8898d94e9c5e76377bd143700088b3dd846
john alejandro <john.e.alejandro01@gmail.com>
2022-01-06 16:31:12 -0600

    created user class and userrespository' git push origin Main

--- src/main/java/com/codeup/springblog/controllers/CardioEquipmentController.java
+++ src/main/java/com/codeup/springblog/controllers/CardioEquipmentController.java
@@ -1,12 +1,8 @@
 package com.codeup.springblog.controllers;
 
-import com.codeup.springblog.models.CardioEquipment;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-
-import static com.codeup.springblog.models.CardioEquipment.deleteTitle;
 
 @Controller
 public class CardioEquipmentController {
--- src/main/java/com/codeup/springblog/controllers/CardioEquipmentRepository.java
+++ src/main/java/com/codeup/springblog/controllers/CardioEquipmentRepository.java
@@ -1,6 +1,5 @@
 package com.codeup.springblog.controllers;
 
-import com.codeup.springblog.models.CardioEquipment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
